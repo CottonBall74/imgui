11e227588b626f1032afa4e3e3f28222155cb832
Cotton Ball <64028676+CottonBall74@users.noreply.github.com>
2023-10-05 23:05:49 +0530

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,8 @@
 project "imgui"
     kind "StaticLib"
     language "C++"
+    cppdialect "C++17"
+    staticruntime "On"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -21,8 +23,11 @@ project "imgui"
 
     filter "system:windows"
         systemversion "latest"
-        cppdialect "C++17"
-        staticruntime "On"
 
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "On"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "On"
